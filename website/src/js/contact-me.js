41e5735c5e190b00ed308cced59d956986d53acb
Yasser <yjeafar@gmail.com>
2020-07-14 15:06:19 -0400

    added styles to contact me page, trying to get validation for form to work properly

--- website/src/js/contact-me.js
+++ website/src/js/contact-me.js
@@ -4,69 +4,131 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
+import { Card, CardContent } from '@material-ui/core';
+import '../css/contact-me.css'
+
 
 const useStyles = makeStyles((theme) => ({
   margin: {
     margin: theme.spacing(1),
   },
+  card: {
+    maxWidth: 420,
+    marginTop: 50
+  },
+  submitButton: {
+    marginTop: 10,
+    float: "right"
+  },
 }));
+
+const initialValues = {
+  name: '',
+  email: '',
+  message: '',
+  errors: {
+    name: '',
+    email: '',
+    message: ''
+  }
+};
 
 
 export default function ContactMe() {
 
   const classes = useStyles();
 
-  function getErrorMessage() {
+  function getEmailError() {
+    return "You suck"
+  }
 
+  function getEmptyError() {
+    return "You suck"
+  }
+
+  function handleSubmit() {
+    return '';
+  }
+  function handleSubmit() {
+    return '';
+  }
+
+  function handleChange () {
+    return '';
+  }
+
+  function handleBlur() {
+    return  '';
   }
 
   return (
     <div className="ContactMe">
-      <form className={classes.root} noValidate autoComplete="off">
-        <div className={classes.margin}>
-          <Grid container spacing={1} alignItems="flex-end">
-            <Grid item>
-              <AccountCircle />
-            </Grid>
-            <Grid item>
-              <TextField
-                id="input-with-icon-grid"
-                label="Name"
-                name= "name"
-              />
-            </Grid>
-          </Grid>
-        </div>
-        <div className={classes.margin}>
-          <Grid container spacing={1} alignItems="flex-end">
-            <Grid item>
-              <AccountCircle />
-            </Grid>
-            <Grid item>
-              <TextField
-                error = { getErrorMessage() }
-                id="input-with-icon-grid"
-                label="Email"
-                name="email"
-              />
-            </Grid>
-          </Grid>
-        </div>
-        <div>
-          <TextField
-            id="outlined-multiline-static"
-            label="Message"
-            multiline
-            rows={6}
-            variant="outlined"
-            name="message"
-          />
-        </div>
-        <Button
-         variant="contained" 
-         color="primary"
-         type="submit"> Submit </Button>
-      </form>
+      <Card className={classes.card}>
+        <CardContent>
+            <form className={classes.root} autoComplete="off" onSubmit={handleSubmit}>
+              <div className={classes.margin}>
+                <Grid container spacing={1} alignItems="flex-end">
+                  <Grid item>
+                    <AccountCircle />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      error={getEmptyError()}
+                      helperText={getEmptyError()}
+                      id="input-with-icon-grid"
+                      label="Name"
+                      name="name"
+                      variant="outlined"
+                      margin="dense"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </Grid>
+                </Grid>
+              </div>
+              <div className={classes.margin}>
+                <Grid container spacing={1} alignItems="flex-end">
+                  <Grid item>
+                    <AccountCircle />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      error={getEmailError}
+                      id="input-with-icon-grid"
+                      label="Email"
+                      name="email"
+                      type="email"
+                      variant="outlined"
+                      margin="dense"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </Grid>
+                </Grid>
+              </div>
+              <div>
+                <TextField
+                  error={getEmptyError}
+                  id="outlined-multiline-static"
+                  label="Message"
+                  name="message"
+                  multiline
+                  rows={10}
+                  rowsMax={10}
+                  variant="outlined"
+                  margin="dense"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </div>
+              <Button
+                className={classes.submitButton}
+                variant="contained"
+                color="primary"
+                type="submit"> Submit </Button>
+            </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
