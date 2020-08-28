a33f02644c37ef6b2e26f8017e0aa4ed891b3cb1
Yasser <yjeafar@gmail.com>
2020-08-27 23:46:07 -0400

    added email functionality

--- src/js/contact-me.js
+++ src/js/contact-me.js
@@ -6,7 +6,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
 import { Card, CardContent } from '@material-ui/core';
 import '../css/contact-me.css'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -41,39 +41,26 @@ export default function ContactMe(props) {
 
   const classes = useStyles();
 
-  let isEmpty = false;
-
-  let errorValues = {};
-
   function getErrorText(label) {
     switch(label) {
       case 'name':
         if (!name) {
-          errorValues.name = "Name can't be empty";
           return("Name can't be empty");
-        }
-        if (name === ' ' && isEmpty) {
-          errorValues.name = "Name can't be empty dumbass"
-          return("Name can't be empty dumbass");
         }
         break;
       case 'email': 
         if (email.length < 1) {
-          errorValues.email =  "Email can't be empty";
           return ("Email can't be empty");
         }
         else if (email !== ' ' && email.length > 1 && !email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)) {
-          errorValues.email = "Invalid email";
           return ('Invalid Email');
         }
         break;
       case 'message':
         if (!message) {
-          errorValues.message = "Message can't be empty";
           return ("Message can't be empty");
         }
         else if (message !== " " && message.length <= 5) {
-          errorValues.message = "Message is too short";
           return ('Message is too short')
         }
         break;
@@ -84,14 +71,13 @@ export default function ContactMe(props) {
     return;
   }
 
-
+  // {user_email: email, user_name: name, message: message }
   function handleSubmit(e) {
       e.preventDefault();
-      console.log('Made it!')
-      isEmpty = true;
-      if (name.length <= 1 && name !== ' ') {
-          getErrorText('name');
-      }
+      window.emailjs.send(
+        'gmail', 'OnlineResume', {message: message, user_name: name, user_email: email}
+      ).then (res =>  <Alert variant="success">Email sent!</Alert>)
+      .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
   }
 
 
@@ -112,7 +98,7 @@ export default function ContactMe(props) {
               <div className={classes.margin}>
                 <Grid container spacing={1} alignItems="flex-end">
                     <TextField
-                      error={ !name || (name === ' ' && isEmpty)  } // Empty name, initialized to be a space so this is not triggered
+                      error={ !name } // Empty name, initialized to be a space so this is not triggered
                       helperText={ getErrorText('name')} // Only show helper text on error
                       id="filled-required"
                       label="Name"
